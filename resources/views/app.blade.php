cbbc43d3f0c79ed3d7dd01ff9dc0f1c98d6bc3ac
Marcelo Araujo Jr <mlajx.ma@gmail.com>
2021-06-15 17:56:06 -0300

    Adicionado o Livewire e configurado o Bootstrap

---
+++ resources/views/app.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Conversão de Moedas</title>
+    <link rel="stylesheet" href="{{mix('css/app.css')}}">
+    <script defer src="{{mix('/js/app.js')}}"></script>
+    @livewireStyles
+</head>
+<body>
+    <div class="container">
+        <div class="col-md-4">
+            <h1>Hello World!</h1>
+        </div>
+    </div>
+    @livewireScripts
+</body>
+</html>
